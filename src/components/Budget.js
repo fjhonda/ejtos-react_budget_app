61a9d0c50540e84a7e9f9c9e69f803c5d11ae43e
Francisco Lemus <fjmorataya1@gmail.com>
2024-03-14 20:19:56 -0400

    Beginning of the Budget component

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,16 @@
+import {AppContext} from '../context/AppContext';
+import { useContext } from 'react';
 
+const Budget = () => {
+
+    const state = useContext(AppContext);
+
+    return (
+        <div className='container'>
+            {`Budget ${state.currency}`}
+            <input type="number" value={state.budget} />
+        </div>
+    )
+}
+
+export default Budget;
